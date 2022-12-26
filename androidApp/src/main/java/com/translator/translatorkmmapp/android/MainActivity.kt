7fd28bc0d31c9eb624995e96e91890a807906d92
Giga99 <igorstevanovic99@gmail.com>
2022-12-26 23:57:53 +0100

    Added colors and theme for Android

--- androidApp/src/main/java/com/translator/translatorkmmapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/translator/translatorkmmapp/android/MainActivity.kt
@@ -4,17 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.translator.translatorkmmapp.Greeting
+import com.translator.translatorkmmapp.android.core.theme.TranslationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
+            TranslationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -34,7 +37,7 @@ fun GreetingView(text: String) {
 @Preview
 @Composable
 fun DefaultPreview() {
-    MyApplicationTheme {
+    TranslationTheme {
         GreetingView("Hello, Android!")
     }
 }
