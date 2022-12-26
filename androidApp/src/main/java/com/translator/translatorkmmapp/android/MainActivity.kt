4088235eb3ed65134d1e9cb58cd96e1e8d99307a
Giga99 <igorstevanovic99@gmail.com>
2022-12-26 23:59:25 +0100

    Deleted Greeting

--- androidApp/src/main/java/com/translator/translatorkmmapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/translator/translatorkmmapp/android/MainActivity.kt
@@ -6,11 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.translator.translatorkmmapp.Greeting
 import com.translator.translatorkmmapp.android.core.theme.TranslationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +18,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
                 }
             }
         }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    TranslationTheme {
-        GreetingView("Hello, Android!")
     }
 }
