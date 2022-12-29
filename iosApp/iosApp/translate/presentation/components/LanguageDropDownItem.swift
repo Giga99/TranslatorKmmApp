ae92760692448296f7f3d87535d628a0173e5cea
Giga99 <igorstevanovic99@gmail.com>
2022-12-30 00:01:21 +0100

    Added theme setup for ios

---
+++ iosApp/iosApp/translate/presentation/components/LanguageDropDownItem.swift
@@ -0,0 +1,21 @@
+//
+//  LanguageDropDownItem.swift
+//  iosApp
+//
+//  Created by Igor Stevanovic on 29.12.22..
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct LanguageDropDownItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LanguageDropDownItem_Previews: PreviewProvider {
+    static var previews: some View {
+        LanguageDropDownItem()
+    }
+}
