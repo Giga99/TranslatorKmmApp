42919317a6b5a4eeda4873867907a09c199e7c05
Giga99 <igorstevanovic99@gmail.com>
2023-01-08 22:47:23 +0100

    Finished voice to text for the ios

--- iosApp/iosApp/translate/presentation/TranslateScreen.swift
+++ iosApp/iosApp/translate/presentation/TranslateScreen.swift
@@ -13,6 +13,7 @@ struct TranslateScreen: View {
     var historyDataSource: HistoryDataSource
     var translateUseCase: Translate
     @ObservedObject var viewModel: IOSTranslateViewModel
+    private let parser = IOSVoiceToTextParser()
     
     init(historyDataSource: HistoryDataSource, translateUseCase: Translate) {
         self.historyDataSource = historyDataSource
@@ -85,7 +86,15 @@ struct TranslateScreen: View {
             
             VStack {
                 Spacer()
-                NavigationLink(destination: Text("Voice-to-text screen")) {
+                NavigationLink(
+                    destination: VoiceToTextScreen(
+                        onResult: { spokenText in
+                            viewModel.onEvent(event: TranslateEvent.SubmitVoiceResult(result: spokenText))
+                        },
+                        parser: parser,
+                        languageCode: viewModel.state.fromLanguage.language.langCode
+                    )
+                ) {
                     ZStack {
                         Circle()
                             .foregroundColor(.primaryColor)
